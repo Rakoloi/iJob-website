4167972444dbc33432a976ad623260208f4a25c9
Mrakoloi <mrakoloi@nwk.co.za>
2026-08-03 16:04:14 +0200

    Configure GitHub Pages base path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages =
+  process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
   output: "export",
+
+  basePath: isGitHubPages
+    ? "/iJob-website"
+    : "",
+
   images: {
     unoptimized: true,
   },
